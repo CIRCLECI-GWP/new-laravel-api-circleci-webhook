b6aee074144fe75495da6da0a0aba312a054123e
yemiwebby <yemiwebby@gmail.com>
2023-06-23 10:54:45 +0100

    Updated php version

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\CircleCIController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
